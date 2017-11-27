a56cae97f83c54c1a9cd605504aec39e73af7b96
Marie Backman <marie.backman@gmail.com>
2017-11-27 14:39:32 +0100

    add movie rating that user can change

--- code.js
+++ code.js
@@ -11,4 +11,24 @@ function renderMovie(movie) {
 
 }
 
+function changeStarRating(grade) {
+    for (let index = 1; index <= 5; index++) {
+        let starId = "star" + index;
+        let starNode = document.getElementById(starId);
+        if (index <= grade) {
+            starNode.classList.add("filledStar");
+        }
+        else {
+            starNode.classList.remove("filledStar");
+        }
+    }
+}
+
+document.getElementById("star1").addEventListener("click", function() { changeStarRating(1); });
+document.getElementById("star2").addEventListener("click", function() { changeStarRating(2); });
+document.getElementById("star3").addEventListener("click", function() { changeStarRating(3); });
+document.getElementById("star4").addEventListener("click", function() { changeStarRating(4); });
+document.getElementById("star5").addEventListener("click", function() { changeStarRating(5); });
+
 renderMovie(movieData);
+changeStarRating(3);
